9ba75b57c4cb4d76870a723547b345c1e41d71f8
Matheus Mauricio <matheus.mauricio@hotmail.com>
2022-09-28 13:31:34 -0300

    Alteração do sistema

---
+++ cadastro_beneficiados.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pilar Social & Projeto Sopa</title>
+    <link href="./css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="./index.php">
+      <img src="./img/imginicio.jpg" alt="Bootstrap" width="60" height="48">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./index.php">Página inicial</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href=""></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Beneficiados
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="./cadastro_beneficiados.php">Cadastro</a></li>
+            <li><a class="dropdown-item" href="./consulta_beneficiados.php">Consulta</a></li>
+            <li><a class="dropdown-item" href="./editar_cadastro.php">Editar Cadastro</a></li>
+            <li><a class="dropdown-item" href="./excluir_cadastro.php">Excluir Cadastro</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./fale_conosco.php">Fale Conosco</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+<div class="container text-center">
+
+<h1>Cadastro beneficiados</h1>
+
+</div>
+
+
+    <script src="./js/bootstrap.bundle.min.js" ></script>
+</body>
+</html>
---
+++ consulta_beneficiados.php
@@ -0,0 +1,1 @@
+<h1>Consultar benficiados</h1>
